644f264a123e84aa430f09c1e5cb86ea1c74cebc
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-04-30 00:14:14 +0000

    一覧表示のルーテングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # 一覧表示のルーティングを設定
+  get 'blogs' => 'blogs#index'
+  
 end
