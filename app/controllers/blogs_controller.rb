6b9243f29e0373ed8864cee765444a417e695b30
EC2 Default User <ec2-user@ip-172-31-33-60.ap-northeast-1.compute.internal>
2020-05-03 05:48:54 +0000

    createアクションとblog_paramsにストロングパラメータを定義した

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,4 +8,15 @@ class BlogsController < ApplicationController
   def new
   end
   
+  # 投稿を保存するcreateアクションを定義
+  def create
+    Blog.create(blog_params)
+  end
+  
+  private
+  # ストロングパラメータを使って、事前に許可したデータのみが保存されるように定義する
+  def blog_params
+    params.permit(:text)
+  end
+  
 end
